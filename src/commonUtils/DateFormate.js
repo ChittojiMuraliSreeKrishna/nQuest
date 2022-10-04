f6081a31587f4b4cb51ee617f8bb4d0c39d00f39
Nagabrunda Kodumuri <nagabrunda.kodumuri@otsi.co.in>
2022-10-04 11:36:31 +0530

    bug fixing

--- src/commonUtils/DateFormate.js
+++ src/commonUtils/DateFormate.js
@@ -1,4 +1,20 @@
-export function formatDate (d) {
-    let date = new Date(d)
-    return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes()+":"+date.getSeconds();
+export function formatDate(d) {
+  let date = new Date(d)
+  return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+export function formatMonth(number) {
+  if (number < 10) {
+    return "-0" + number;
+  } else {
+    return "-" + number;
+  }
+}
+
+export function dateFormat(number) {
+  if (number < 10) {
+    return "-0" + number;
+  } else {
+    return "-" + number
+  }
 }
